3166eceddfba1d9d43a58c3314ab42d5528123b1
PrathamManabasannanavar <pratham29052004@gmail.com>
2025-07-30 12:18:08 +0530

    fixed origin

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 // CORS middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT'],
   allowedHeaders: ['Content-Type']
