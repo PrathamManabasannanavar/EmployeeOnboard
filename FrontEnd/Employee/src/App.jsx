57761792dd7d74a34ed313ee051514544215d151
PrathamManabasannanavar <pratham29052004@gmail.com>
2025-07-30 13:36:16 +0530

    added files5 frontend

--- FrontEnd/Employee/src/App.jsx
+++ FrontEnd/Employee/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom"
+import {HashRouter as Router, Route, Routes, Link} from "react-router-dom"
 import LoginPage from "./Components/LoginPage"
 import NavBar from "./Components/NavBar"
 import SpaceBox from "./Components/SpaceBox"
