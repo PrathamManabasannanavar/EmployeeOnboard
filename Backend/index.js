a748262b4546ef8f86aa3aed157d17c609d90294
PrathamManabasannanavar <pratham29052004@gmail.com>
2025-07-30 13:25:09 +0530

    fixed origin for vercel

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 // CORS middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://employee-onboard-mv6rq2evv-prathammanabasannanavars-projects.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT'],
   allowedHeaders: ['Content-Type']
